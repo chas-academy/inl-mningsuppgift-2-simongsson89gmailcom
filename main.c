be95a808bf37a6141c2143ddd1e714085cdc5d09
simon.gsson89@gmail.com <simon.gsson89@gmail.com>
2025-05-21 21:58:46 +0200

    Uppdaterade main.c

--- main.c
+++ main.c
@@ -2,7 +2,15 @@
 
 
 // This is where you add the two functions
+float fahrenheit_to_celsius(float fahr)
+{
+    return (float)((fahr-32)*5)/9;
+}
 
+float celsius_to_fahrenheit(float cels)
+{
+    return (float)((cels*9)/5+32);
+}
 int main() {
     float temp_f = 100.0;
     float temp_c = 37.0;
